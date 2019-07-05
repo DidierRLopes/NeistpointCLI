5a42acba0aff4762debce68f6bc78961cb7fef07
DidierRodriguesLopes <dro.lopes@campus.fct.unl.pt>
2019-07-05 20:01:08 +0100

    Initial Commit

---
+++ neistpointTerminal/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  neistpointTerminal
+//
+//  Created by Didier Rodrigues Lopes on 05/07/2019.
+//  Copyright © 2019 Didier Lopes. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
